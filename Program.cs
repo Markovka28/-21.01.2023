3f288f5b0e279358b6b315a6b29466bc795eb09f
Valery_Krylov <supermaaaan@mail.ru>
2023-01-22 20:59:53 +0500

    решение задачи47(22.01.2023)

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
-Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
-
-//int[]arr=Enumerable.Range(-100,100).ToArray(); c помощью диапазона
-int[] arr = { 3, -2, -1, 4, 1, -2, 3, -8, 56 }; // с помощью заданных значений
-var A = 0;
-foreach (var x in arr)
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными 
+вещественными числами.*/
+double[,] mas = new double[5, 5];
+Random random = new Random();
+for (int i = 0; i < mas.GetLength(0); i++)
 {
-    if (x>0)
-    {
-        A++;
-    }
+for (int j = 0; j < mas.GetLength(1); j++) 
+mas[i,j] = random.NextDouble() * 100; 
 }
-Console.Write($"Введено {A} чисел больше нуля");
-
-
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+for (int j = 0; j < mas.GetLength(1); j++)
+Console.Write("{0,8:F}", mas[i,j]); 
+Console.WriteLine(); 
+}
+ 
