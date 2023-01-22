5a363b134d292347f157edd6f17f6fcdce412fb3
Valery_Krylov <supermaaaan@mail.ru>
2023-01-22 22:40:35 +0500

    решение задачи50(22.01.2023)

--- Program.cs
+++ Program.cs
@@ -1,16 +1,42 @@
-﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными 
-вещественными числами.*/
-double[,] mas = new double[5, 5];
-Random random = new Random();
-for (int i = 0; i < mas.GetLength(0); i++)
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном 
+массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colums = int.Parse(Console.ReadLine());
+int[,] numbers = new int[5, 5];
+FillArray(numbers);
+PrintArray(numbers);
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) 
 {
-for (int j = 0; j < mas.GetLength(1); j++) 
-mas[i,j] = random.NextDouble() * 100; 
+    Console.WriteLine(numbers[rows, colums]);
 }
-for (int i = 0; i < mas.GetLength(0); i++)
+else Console.WriteLine($"{rows}{colums} такого числа в массиве нет");
+void FillArray(int[,] array)
 {
-for (int j = 0; j < mas.GetLength(1); j++)
-Console.Write("{0,8:F}", mas[i,j]); 
-Console.WriteLine(); 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 33);
+        }
+    }
 }
- 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+} 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
